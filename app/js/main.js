dc8212b14643e5979717ee9921e5a901dec051e9
Liuda Arkavenko <arkavenko18@gmail.com>
2021-07-09 18:31:17 +0300

    final commit

--- app/js/main.js
+++ app/js/main.js
@@ -7,5 +7,8 @@ $(function () {
         prevArrow: '<button type="button" class="slider__arrow slider__arrowleft"><img src="images/slider-arrow-left.svg" alt="arrow"></button>',
         nextArrow: '<button type="button" class="slider__arrow slider__arrowright"><img src="images/slider-arrow-right.svg" alt="arrow"></button>',
     });
-    
+    $(".footer__scroll-btn").click(function() {
+        $("html, body").animate({ scrollTop: 0 }, "slow");
+        return false;
+    });    
 })
